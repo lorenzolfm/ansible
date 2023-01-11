fff1c326b8a57430e5a5e1fcbade7402e7ca49f3
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-01-11 20:27:53 -0300

    feat: covering all that ansible does for ssh files

--- tests/ssh.sh
+++ tests/ssh.sh
@@ -6,3 +6,6 @@ directory="/home/lorenzo/.ssh"
 echo -e "\nTesting ssh setup...\n"
 
 test_directory_owner_group_and_permissions "/home/lorenzo/.ssh" "lorenzo" "lorenzo" 700
+test_file_owner_group_and_permissions "/home/lorenzo/.ssh/id_rsa" "lorenzo" "lorenzo" 600
+test_file_owner_group_and_permissions "/home/lorenzo/.ssh/id_rsa.pub" "lorenzo" "lorenzo" 644
+test_file_owner_group_and_permissions "/home/lorenzo/.ssh/known_hosts" "lorenzo" "lorenzo" 644
