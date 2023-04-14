73c142142bd5a6c7b62f9dfa9db6931ea9c2079d
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-04-14 17:25:12 -0300

    Running all tests

--- tests/run_all_tests.sh
+++ tests/run_all_tests.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
 source ./tests/dockerfile.sh
-source ./tests/ssh.sh
+source ./tests/docker.sh
 source ./tests/dotfiles.sh
 source ./tests/languages.sh
+source ./tests/ssh.sh
 source ./tests/zsh.sh
 source ./tests/tmux.sh
-source ./tests/misc.sh
 source ./tests/neovim.sh
+source ./tests/misc.sh
