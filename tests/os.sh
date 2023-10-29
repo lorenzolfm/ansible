f6f847982e2c883e7a5daded8a58f45f546f2b3a
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-10-29 16:47:24 -0300

    chore: rename file

--- tests/os.sh
+++ tests/os.sh
@@ -4,3 +4,4 @@ source ./tests/utils.sh
 echo -e "\nTesting the i3 task\n"
 
 assert_exits_has_correct_owner_group_and_permissions "/usr/bin/i3" "root" "root" 755
+assert_exits_has_correct_owner_group_and_permissions "/usr/bin/picom" "root" "root" 755
