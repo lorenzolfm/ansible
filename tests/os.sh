de89a4080a30a310b39a4c8bed96f7799d1e6c64
Lorenzo Maturano <maturanolorenzo@gmail.com>
2023-10-29 17:26:18 -0300

    feat: os update

--- tests/os.sh
+++ tests/os.sh
@@ -5,3 +5,6 @@ echo -e "\nTesting the i3 task\n"
 
 assert_exits_has_correct_owner_group_and_permissions "/usr/bin/i3" "root" "root" 755
 assert_exits_has_correct_owner_group_and_permissions "/usr/bin/picom" "root" "root" 755
+assert_exits_has_correct_owner_group_and_permissions "/usr/bin/rofi" "root" "root" 755
+assert_exits_has_correct_owner_group_and_permissions "/usr/bin/polybar" "root" "root" 755
+assert_exits_has_correct_owner_group_and_permissions "/usr/bin/playerctl" "root" "root" 755
